7f89c576c9f63439031411646e7d9726ddab1ffa
Dylan Price <the.dylan.price@gmail.com>
2012-07-17 19:57:22 -0700

    Renamed src to hutmap.
    --HG--
    rename : src/__init__.py => hutmap/__init__.py
    rename : src/manage.py => hutmap/manage.py
    rename : src/polls/__init__.py => hutmap/polls/__init__.py
    rename : src/polls/admin.py => hutmap/polls/admin.py
    rename : src/polls/models.py => hutmap/polls/models.py
    rename : src/polls/tests.py => hutmap/polls/tests.py
    rename : src/polls/urls.py => hutmap/polls/urls.py
    rename : src/polls/views.py => hutmap/polls/views.py
    rename : src/settings.py => hutmap/settings.py
    rename : src/settings_development.py => hutmap/settings_development.py
    rename : src/settings_local.py.dist => hutmap/settings_local.py.dist
    rename : src/settings_production.py => hutmap/settings_production.py
    rename : src/templates/404.html => hutmap/templates/404.html
    rename : src/templates/admin/base_site.html => hutmap/templates/admin/base_site.html
    rename : src/templates/polls/poll_detail.html => hutmap/templates/polls/poll_detail.html
    rename : src/templates/polls/poll_list.html => hutmap/templates/polls/poll_list.html
    rename : src/templates/polls/results.html => hutmap/templates/polls/results.html
    rename : src/urls.py => hutmap/urls.py

--- hutmap/urls.py
+++ hutmap/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 
-from django.contrib import admin
+from django.contrib.gis import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
