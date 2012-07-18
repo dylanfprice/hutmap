404d03efaa9b512e702c4c24dbbb053e64c6af57
Dylan Price <the.dylan.price@gmail.com>
2012-07-18 00:00:13 -0700

    Added index page.

--- hutmap/urls.py
+++ hutmap/urls.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from django.contrib.gis import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^polls/', include('polls.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+urlpatterns = patterns('django.views.generic.simple',
+  (r'^$',  'direct_to_template', {'template': 'index.html'}),
 )
 
+urlpatterns += patterns('',
+  (r'^polls/',      include('polls.urls')),
+  (r'^admin/',      include(admin.site.urls)),
+  (r'^admin/doc/',  include('django.contrib.admindocs.urls')),
+)
+
+# Serve static files through django if in debug
 if settings.DEBUG:
   urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 
