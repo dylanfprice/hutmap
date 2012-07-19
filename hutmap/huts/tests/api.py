f470317a3aa6099014cf85f4baff8c91601e0fda
Dylan Price <the.dylan.price@gmail.com>
2012-07-19 01:39:37 -0700

    Updated unit tests.

--- hutmap/huts/tests/api.py
+++ hutmap/huts/tests/api.py
@@ -5,7 +5,15 @@ class HutResourceTestCase(TestCase):
   fixtures = ['test_data.json']
 
   def test_query_bbox(self):
-    response = self.client.get('/huts/api/v1/hut/?bbox=-122.79683,47.05814,-120.79683,49.05814')
+    response = self.client.get('/huts/api/v1/hut/?bbox=47.05814,-122.79683,49.05814,-120.79683')
     results = json.loads(response.content)
     self.assertEqual(7, results['meta']['total_count'])
+
+  def test(self):
+    response = self.client.get('/huts/api/v1/hut/?bbox=45.795034%2C-124.838039%2C49.636264%2C-116.642238') 
+    results = json.loads(response.content)
+    print(results)
+
+
+
 
