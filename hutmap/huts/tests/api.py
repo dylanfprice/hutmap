2f7c53b6f01e4763248e3dd5f36cf09781043a1d
Dylan Price <the.dylan.price@gmail.com>
2012-07-19 01:39:58 -0700

    Updated test.

--- hutmap/huts/tests/api.py
+++ hutmap/huts/tests/api.py
@@ -9,11 +9,3 @@ class HutResourceTestCase(TestCase):
     results = json.loads(response.content)
     self.assertEqual(7, results['meta']['total_count'])
 
-  def test(self):
-    response = self.client.get('/huts/api/v1/hut/?bbox=45.795034%2C-124.838039%2C49.636264%2C-116.642238') 
-    results = json.loads(response.content)
-    print(results)
-
-
-
-
