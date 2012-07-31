78e0684851ccc58add4930858bb5d56125f20fc5
Dylan Price <the.dylan.price@gmail.com>
2012-07-31 12:26:35 -0700

    Added function doc.

--- hutmap/huts/context_processors.py
+++ hutmap/huts/context_processors.py
@@ -1,4 +1,5 @@
 
 def settings(request):
+  """ Add extra variables to RequestContext objects for use in templates. """
   from django.conf import settings
   return {'DEVELOPMENT': settings.DEVELOPMENT}
