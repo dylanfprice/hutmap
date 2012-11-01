c4f67b13d01e5ffd9597737605b6e25091e78ea0
Dylan Price <the.dylan.price@gmail.com>
2012-10-31 17:18:12 -0700

    Started a file for generated constants which will eventually be generated from the django models.

---
+++ static/js/hutmap/gconsts.js
@@ -0,0 +1,12 @@
+/**
+ * Constants from the server-side of things.
+ *
+ * These are currently hand-generated from hutmap/huts/models.py, but in the
+ * future there should be a script.
+ */
+hutmap.consts.g = {
+  access: ['frontcountry year-round', 'winter backcountry',
+  'backcountry year-round'],
+  maxAccuracy: 4
+};
+
