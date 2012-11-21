e08a83cc0575f20b4b0351a3aa79b0573719af9c
Dylan Price <the.dylan.price@gmail.com>
2012-11-21 15:32:02 -0800

    Added hut field names to gconsts.

--- static/js/hutmap/gconsts.js
+++ static/js/hutmap/gconsts.js
@@ -7,6 +7,29 @@
 hutmap.consts.g = {
   access: ['frontcountry year-round', 'winter backcountry',
   'backcountry year-round'],
-  maxAccuracy: 4
+  maxAccuracy: 4,
+  hut: {
+    created: "created",
+    updated: "updated",
+    region: "region",
+    location: "location",
+    location_accuracy: "location_accuracy",
+    altitude: "altitude",
+    name: "name",
+    access: "access",
+    type: "type",
+    num_structures: "num_structures",
+    capacity_max: "capacity_max",
+    capacity_hut_max: "capacity_hut_max",
+    capacity_hut_min: "capacity_hut_min",
+    fee_person_min: "fee_person_min",
+    fee_person_max: "fee_person_max",
+    fee_hut_min: "fee_hut_min",
+    fee_hut_max: "fee_hut_max",
+    reservations: "reservations",
+    hut_url: "hut_url",
+    photo_url: "photo_url",
+    references: "references",
+    agency: "agency"
+  }
 };
-
