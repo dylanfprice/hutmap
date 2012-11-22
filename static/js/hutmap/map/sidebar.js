baf4a727a1b633658aa949047c8c36803aef6bb1
Dylan Price <the.dylan.price@gmail.com>
2012-11-21 16:44:28 -0800

    Changed references to hut fields to use gconsts.

--- static/js/hutmap/map/sidebar.js
+++ static/js/hutmap/map/sidebar.js
@@ -16,6 +16,7 @@ goog.require('Wkt.gmap3');
  */
 hutmap.map.Sidebar = function(sidebarContentDivId) {
   this.sidebarDiv = goog.dom.getElement(sidebarContentDivId);
+  this.h = hutmap.consts.g.hut;
 };
 
 /**
@@ -28,23 +29,24 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   var textContent = '';
   // Set the title
   var name = goog.object.get(hut, 'name', 'Unknown hut');
-  var hutUrl = goog.object.get(hut, 'hut_url', '');
+  var hutUrl = goog.object.get(hut, this.h.hut_url, '');
   textContent = '<a href="' + hutUrl + '" target="blank">' + name + '</a>';
   elt = goog.dom.getElement('sidebar_title');
   elt.innerHTML = textContent;
   // Set the image
-  var photoUrl = goog.object.get(hut, 'photo_url');
+  var photoUrl = goog.object.get(hut, this.h.photo_url);
   if (photoUrl === '' || photoUrl == null)
     photoUrl = '/static/img/no-image-found.jpeg';
   textContent = '<a href="' + hutUrl + '" target="blank"><img src="' + photoUrl + '" alt="No photo"></img></a>';
   elt = goog.dom.getElement('sidebar_img');
   elt.innerHTML = textContent;
   // Set the fee per person
-  textContent = this.getFeeRange(hut, 'fee_person_min', 'fee_person_max');
+  textContent = this.getFeeRange(hut, this.h.fee_person_min,
+      this.h.fee_person_max);
   elt = goog.dom.getElement('sidebar_fee_person');
   goog.dom.setTextContent(elt, textContent);
   // Set the fee per hut
-  textContent = this.getFeeRange(hut, 'fee_hut_min', 'fee_hut_max');
+  textContent = this.getFeeRange(hut, this.h.fee_hut_min, this.h.fee_hut_max);
   elt = goog.dom.getElement('sidebar_fee_hut');
   goog.dom.setTextContent(elt, textContent);
   // Set the hut capacity
@@ -52,19 +54,19 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_capacity_hut');
   goog.dom.setTextContent(elt, textContent);
   // Set the total capacity
-  textContent = goog.object.get(hut, 'capacity_max', 'Unknown');
+  textContent = goog.object.get(hut, this.h.capacity_max, 'Unknown');
   elt = goog.dom.getElement('sidebar_total_capacity');
   goog.dom.setTextContent(elt, textContent);
   // Set the number of structures
-  textContent = goog.object.get(hut, 'num_structures', 'Unknown');
+  textContent = goog.object.get(hut, this.h.num_structures, 'Unknown');
   elt = goog.dom.getElement('sidebar_structures');
   goog.dom.setTextContent(elt, textContent);
   // Set whether reservations are accepted
-  textContent = this.getBooleanAsYesNo(hut, 'reservations');
+  textContent = this.getBooleanAsYesNo(hut, this.h.reservations);
   elt = goog.dom.getElement('sidebar_reservations');
   goog.dom.setTextContent(elt, textContent);
   // Set the region
-  var region = goog.object.get(hut, 'region', null);
+  var region = goog.object.get(hut, this.h.region, null);
   if (region == null) {
     textContent = 'Unknown';
   } else {
@@ -73,7 +75,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_region');
   goog.dom.setTextContent(elt, textContent);
   // Set the location
-  var location = goog.object.get(hut, 'location', null);
+  var location = goog.object.get(hut, this.h.location, null);
   if (location == null) {
     textContent = 'Unknown';
   } else {
@@ -83,7 +85,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_location');
   goog.dom.setTextContent(elt, textContent);
   // Set the accuracy
-  var locationAccuracy = goog.object.get(hut, 'location_accuracy', null);
+  var locationAccuracy = goog.object.get(hut, this.h.location_accuracy, null);
   if (locationAccuracy == null) {
     textContent = 'Unknown';
   } else {
@@ -93,7 +95,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_accuracy');
   goog.dom.setTextContent(elt, textContent);
   // Set the altitude
-  var altitude = goog.object.get(hut, 'altitude', null);
+  var altitude = goog.object.get(hut, this.h.altitude, null);
   if (altitude == null) {
     textContent = 'Unknown';
   } else {
@@ -102,7 +104,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_altitude');
   goog.dom.setTextContent(elt, textContent);
   // Set the access
-  var access = goog.object.get(hut, 'access', null);
+  var access = goog.object.get(hut, this.h.access, null);
   if (access == null) {
     textContent = 'Unknown';
   } else {
@@ -120,7 +122,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_type');
   goog.dom.setTextContent(elt, textContent);
   // Set the agency
-  var agency = goog.object.get(hut, 'agency', null);
+  var agency = goog.object.get(hut, this.h.agency, null);
   if (agency == null) {
     textContent = 'Unknown';
   } else {
@@ -130,7 +132,7 @@ hutmap.map.Sidebar.prototype.setHut = function(hut) {
   elt = goog.dom.getElement('sidebar_agency');
   elt.innerHTML = textContent;
   // Set the url
-  var url = goog.object.get(hut, 'hut_url', null);
+  var url = goog.object.get(hut, this.h.hut_url, null);
   if (url == null) {
     textContent = 'Unknown';
   } else {
@@ -170,8 +172,8 @@ hutmap.map.Sidebar.prototype.getFeeRange = function(hut, feeMinProp, feeMaxProp)
  * @private
  */
 hutmap.map.Sidebar.prototype.getCapacityRange = function(hut) {
-  var capMin = hut['capacity_hut_min'];
-  var capMax = hut['capacity_hut_max'];
+  var capMin = hut[this.h.capacity_hut_min];
+  var capMax = hut[this.h.capacity_hut_max];
 
   var textContent = capMin + '-' + capMax;
   if (capMin == null || capMax == null) {
