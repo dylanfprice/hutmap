d76f457d453c88e240a9e20d92be48f47339360b
Dylan Price <the.dylan.price@gmail.com>
2013-01-25 20:54:41 -0800

    Added soy templates.'

---
+++ src/js/hutmap/templates.js
@@ -0,0 +1,18 @@
+// This file was automatically generated from templates.soy.
+// Please don't edit this file by hand.
+
+goog.provide('hutmap.templates');
+
+goog.require('soy');
+goog.require('soydata');
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @return {string}
+ * @notypecheck
+ */
+hutmap.templates.helloWorld = function(opt_data, opt_ignored) {
+  return 'Hello world!';
+};
