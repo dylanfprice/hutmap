f7b2096d6bfc2cecf35725ecb543bd4c94442a45
Dylan Price <the.dylan.price@gmail.com>
2013-01-25 20:54:05 -0800

    Got rid of camel case.

--- src/js-test/hutmap/filters_test.js
+++ src/js-test/hutmap/filters_test.js
@@ -3,41 +3,43 @@ goog.require('hutmap.filters');
 goog.require('goog.testing.jsunit');
 
 hut = {
-  priceHutMin: 6,
-  priceHutMax: 12,
+  price_hut_min: 6,
+  price_hut_max: 12,
 };
 
-var testCreatePriceHutFilterRangeMatches = function() {
-  var f = hutmap.filters.createPriceHutFilter(6, 12);
+/** create_price_hut_filter **/
+
+var test_create_price_hut_filter_range_matches = function() {
+  var f = hutmap.filters.create_price_hut_filter(6, 12);
   assertTrue(f(hut));
 };
 
-var testCreatePriceHutFilterRangeWithin = function() {
-  var f = hutmap.filters.createPriceHutFilter(7, 10);
+var test_create_price_hut_filter_range_within = function() {
+  var f = hutmap.filters.create_price_hut_filter(7, 10);
   assertTrue(f(hut));
 };
 
-var testCreatePriceHutFilterRangeContains = function() {
-  var f = hutmap.filters.createPriceHutFilter(5, 15);
+var test_create_price_hut_filter_range_contains = function() {
+  var f = hutmap.filters.create_price_hut_filter(5, 15);
   assertTrue(f(hut));
 };
 
-var testCreatePriceHutFilterRangeLowerOverlapping = function() {
-  var f = hutmap.filters.createPriceHutFilter(3, 10);
+var test_create_price_hut_filter_range_lower_overlapping = function() {
+  var f = hutmap.filters.create_price_hut_filter(3, 10);
   assertTrue(f(hut));
 };
 
-var testCreatePriceHutFilterRangeHigherOverlapping = function() {
-  var f = hutmap.filters.createPriceHutFilter(8, 15);
+var test_create_price_hut_filter_range_higher_overlapping = function() {
+  var f = hutmap.filters.create_price_hut_filter(8, 15);
   assertTrue(f(hut));
 };
 
-var testCreatePriceHutFilterRangeBelow = function() {
-  var f = hutmap.filters.createPriceHutFilter(1, 5);
+var test_create_price_hut_filter_range_below = function() {
+  var f = hutmap.filters.create_price_hut_filter(1, 5);
   assertFalse(f(hut));
 };
 
-var testCreatePriceHutFilterRangeAbove = function() {
-  var f = hutmap.filters.createPriceHutFilter(13, 15);
+var test_create_price_hut_filter_range_above = function() {
+  var f = hutmap.filters.create_price_hut_filter(13, 15);
   assertFalse(f(hut));
 };
