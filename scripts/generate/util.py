a4f47ffe3aab52257896cb7b86e2155ccdae945f
Dylan Price <the.dylan.price@gmail.com>
2013-02-16 12:13:05 -0800

    Added method to write out to file.
    --HG--
    branch : js-redesign

--- scripts/generate/util.py
+++ scripts/generate/util.py
@@ -2,4 +2,19 @@ import imp
 from os.path import normpath, join, dirname
 
 def get_config():
+  """Returns an object containing the config in scripts/config.py"""
   return imp.load_source('config', normpath(join(dirname(__file__), '../config.py')))
+
+def write_file(string, path, remove_blank_lines=False):
+  """
+  Write the given string to the file at path. If remove_blank_lines is True,
+  will not write any lines that are empty or contain only whitespace.
+  """
+  with open(path, 'w') as out:
+    for line in string.splitlines():
+      is_blank = not line or line.isspace()
+      should_write = not remove_blank_lines or not is_blank
+      if should_write:
+        out.write(line)
+        out.write('\n')
+
