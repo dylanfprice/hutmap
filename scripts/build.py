09ec7fe5871948c8a0f2978ef3f4bb0d6accebe1
Dylan Price <the.dylan.price@gmail.com>
2013-02-16 12:25:33 -0800

    Cleaned up build.py for new build scheme.
    --HG--
    branch : js-redesign

--- scripts/build.py
+++ scripts/build.py
@@ -1,26 +1,22 @@
-#!/usr/bin/python
 # Compiles javascript and copies js, css, and img files to the public folder of
 # the hutmap website.
 
+from os.path import join
 import config
-
+import os
 import shutil
 import subprocess
-import os
-from os.path import join
 
+def set_permissions():
+  """Sets permissions in public folder so Apache will serve files."""
+  for dirpath,dirnames,filenames in os.walk(config.PUBLIC_PATH):
+    os.chmod(dirpath, 0755)
+    for filename in filenames:
+      path = join(dirpath, filename)
+      os.chmod(path, 0755)
 
-def compile_soy_templates():
-  subprocess.check_call(
-      ['java', '-jar',
-        config.CLOSURE_TEMPLATES,
-        '--shouldGenerateJsdoc',
-        '--shouldProvideRequireSoyNamespaces',
-        '--srcs', '{0}'.format(join(config.JS_PATH, 'hutmap', 'templates.soy')),
-        '--outputPathFormat', '{0}'.format(join(config.JS_PATH, 'hutmap', 'templates.js'))])
-  print('Successfully compiled soy templates')
-
-def compile_js():
+def build_js():
+  """Compiles javascript using closure"""
   closure_builder = join(config.CLOSURE_LIBRARY, 'closure', 
                          'bin', 'build', 'closurebuilder.py')
 
@@ -39,24 +35,14 @@ def compile_js():
      '--output_mode=compiled',
      '--compiler_jar={0}'.format(config.CLOSURE_COMPILER),
      '--output_file={0}'.format(join(config.JS_DEST, 'hutmap-compiled.js'))])
-  print('Successfully compiled javascript.')
 
-def copy_css():
+  set_permissions()
+
+def build_css():
+  """Copies css files into public/css"""
   # TODO: minify css
   shutil.rmtree(config.CSS_DEST, ignore_errors=True)
   shutil.copytree(config.CSS_PATH, config.CSS_DEST)
-  print('Successfully copied css')
-
-def set_permissions():
-  for dirpath,dirnames,filenames in os.walk(config.PUBLIC_PATH):
-    os.chmod(dirpath, 0755)
-    for filename in filenames:
-      path = join(dirpath, filename)
-      os.chmod(path, 0755)
-  print('Successfully set permissions')
-
-if __name__ == '__main__':
-  compile_soy_templates()
-  compile_js()
-  copy_css()
   set_permissions()
+
+
