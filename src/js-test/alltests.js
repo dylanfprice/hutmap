fdb3a13b4b663b608014f0f64f29b762f7e2a5df
Dylan Price <the.dylan.price@gmail.com>
2013-02-16 13:47:09 -0800

    Added generated alltests.js.
    --HG--
    branch : js-redesign

---
+++ src/js-test/alltests.js
@@ -0,0 +1,6 @@
+// This file generated from the template at scripts/make/generate/templates/alltests.js
+// Please do not edit by hand.
+_allTests = [
+  "hutmap/filters_test.html",
+  "hutmap/templates_test.html",
+];
