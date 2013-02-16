a2c55b774ed324235a25cd27c31dba9e6ab4b8a1
Dylan Price <the.dylan.price@gmail.com>
2013-02-16 12:13:57 -0800

    Added BASE_PATH to config.
    --HG--
    branch : js-redesign

--- scripts/config.py
+++ scripts/config.py
@@ -1,21 +1,26 @@
-#
-
-import os
 from os.path import join, dirname, normpath
+import os
 
 CLOSURE_LIBRARY = os.environ['HUTMAP_CLOSURE_LIBRARY']
 CLOSURE_COMPILER = os.environ['HUTMAP_CLOSURE_COMPILER']
 CLOSURE_TEMPLATES = os.environ['HUTMAP_CLOSURE_TEMPLATES']
 
 LOCAL_PATH = normpath(join(dirname(__file__), '.'))
-SRC_PATH = join(LOCAL_PATH, '..', 'src')
+BASE_PATH = join(LOCAL_PATH, '..')
+SRC_PATH = join(BASE_PATH, 'src')
 
 CSS_PATH = join(SRC_PATH, 'css')
 HUTMAP_PATH = join(SRC_PATH, 'hutmap')
 JS_PATH = join(SRC_PATH, 'js')
 JS_TEST_PATH = join(SRC_PATH, 'js-test')
 
-PUBLIC_PATH = join(LOCAL_PATH, '..', 'public')
+PUBLIC_PATH = join(BASE_PATH, 'public')
 JS_DEST = join(PUBLIC_PATH, 'static', 'js')
 CSS_DEST = join(PUBLIC_PATH, 'static', 'css')
 
+# for later
+#generate_models([
+#    {'name': 'Hut',    'skip_fields': ['created', 'updated']},
+#    {'name': 'Agency', 'skip_fields': []},
+#    {'name': 'Region', 'skip_fields': []},
+#  ])
