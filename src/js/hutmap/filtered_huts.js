0ca478f7467f71bb07c2123fdca20a33eba99f55
Dylan Price <the.dylan.price@gmail.com>
2013-02-20 22:25:01 -0800

    Implemented FilteredHuts.
    --HG--
    branch : js-redesign

--- src/js/hutmap/filtered_huts.js
+++ src/js/hutmap/filtered_huts.js
@@ -13,6 +13,7 @@ goog.scope(function() {
   var assert = goog.asserts.assert;
   var assertArray = goog.asserts.assertArray;
   var assertInstanceof = goog.asserts.assertInstanceof;
+  var assertNumber = goog.asserts.assertNumber;
   var assertObject = goog.asserts.assertObject;
   var assertString = goog.asserts.assertString;
   var object = goog.object;
@@ -28,16 +29,8 @@ goog.scope(function() {
    * @constructor
    */
   hutmap.FilteredHuts = function(huts, filters) {
-    assertArray(huts);
-    this.huts = [];
-    array.forEach(huts, function(hut, index, array) {
-      var id = hut.id;
-      assert(id != null, "FilteredHuts(): all given huts must have an id.");
-      this.huts[id] = hut;  
-    }, this);
-    
+    this._set_huts(assertArray(huts));
     this.filters = new structs.Map(assertObject(filters));
-
     this.recompute_filtered_huts = true;
     this.filtered_huts = [];
 
@@ -50,13 +43,14 @@ goog.scope(function() {
 
   hutmap.FilteredHuts.prototype._check_rep = function() {
     array.forEach(this.huts, function(hut, index, array) {
-      assertInstanceof(hut, hutmap.Hut);
-      assert(index === hut.id);
+      assertInstanceof(hut, hutmap.Hut, "hut not an instance of hutmap.Hut");
+      assert(index === hut.id, "index of array not the hut id");
     }, this);
 
     structs.forEach(this.filters, function(filter, field, filters) {
-      assertString(field);
-      assertInstanceof(filter, hutmap.Filter);
+      assertString(field, "field not a string");
+      assertInstanceof(filter, hutmap.Filter, 
+        "filter not an instance of hutmap.Filter");
     }, this);
   };
 
@@ -79,24 +73,6 @@ goog.scope(function() {
     return object.createImmutableView(this.filtered_huts);
   };
 
-  hutmap.FilteredHuts.prototype._compute_filtered_huts = function() {
-    this.filtered_huts = [];
-    array.forEach(this.huts, function(hut, index, huts) {
-      if (this._apply_filters(hut)) {
-        this.filtered_huts.push(hut);
-      }
-    }, this);
-  };
-
-  hutmap.FilteredHuts.prototype._apply_filters = function(hut) {
-    return structs.every(this.filters, 
-    function(filter, field, filters) {
-      field_array = field.split('.');
-      var value = object.getValueByKeys(hut, field_array);
-      return filter.filter(value);
-    }, this);
-  };
-
   /**
    * @return {number} the number of huts which match all the filters.
    */
@@ -107,14 +83,40 @@ goog.scope(function() {
     return this.filtered_huts.length;
   };
 
+  /**
+   * @private
+   */
+  hutmap.FilteredHuts.prototype._compute_filtered_huts = function() {
+    this.filtered_huts = array.filter(this.huts, 
+      function(hut, index, huts) {
+        return this._apply_filters(hut);
+      }, this);
+    this.recompute_filtered_huts = false;
+  };
+
+  /**
+   * @private
+   */
+  hutmap.FilteredHuts.prototype._apply_filters = function(hut) {
+    return structs.every(this.filters, 
+      function(filter, field, filters) {
+        field_array = field.split('.');
+        var value = object.getValueByKeys(hut, field_array);
+        return filter.filter(value);
+      }, this);
+  };
+
   /** Huts */
 
   /**
    * @param id {number} the id of the hut to retrieve
-   * @return {hutmap.Hut} the hut specified by id
+   * @return {hutmap.Hut} the hut specified by id, or null if there is no hut
+   *    with the given id
    */
   hutmap.FilteredHuts.prototype.get_hut = function(id) {
-    throw "not implemented";
+    assertNumber(id);
+    var hut = this.huts[id];
+    return (hut) ? hut : null;
   };
 
   /**
@@ -122,21 +124,29 @@ goog.scope(function() {
    * @return {boolean} true if this contains a hut with given id
    */
   hutmap.FilteredHuts.prototype.contains_hut = function(id) {
-    throw "not implemented";
+    assertNumber(id);
+    return (id in this.huts);
   };
 
   /**
    * @return {Array.<hutmap.Hut> a list of the huts contained in this
    */
   hutmap.FilteredHuts.prototype.get_huts = function() {
-    throw "not implemented";
+    return array.filter(this.huts, function(elt, index, array) {
+        return elt != null;
+      }, this);
   };
 
   /**
    * @return {number} the number of huts contained in this
    */
   hutmap.FilteredHuts.prototype.get_huts_count = function() {
-    throw "not implemented";
+    var count = 0;
+    array.forEach(
+        this.huts, 
+        function(elt, i, array) { count++; },
+        this);
+    return count;
   };
 
   /**
@@ -144,7 +154,10 @@ goog.scope(function() {
    *    the given hut already exists, it will be replaced.
    */
   hutmap.FilteredHuts.prototype.add_hut = function(hut) {
-    throw "not implemented";
+    assertInstanceof(hut, hutmap.Hut);
+    assertNumber(hut.id);
+    this.huts[hut.id] = hut;
+    this._huts_changed();
   };
 
   /**
@@ -152,7 +165,30 @@ goog.scope(function() {
    *    will be cleared.
    */
   hutmap.FilteredHuts.prototype.set_huts = function(huts) {
-    throw "not implemented";
+    assertArray(huts);
+    this._set_huts(huts);
+    this._huts_changed();
+  };
+
+  /**
+   * @private
+   */
+  hutmap.FilteredHuts.prototype._set_huts = function(huts) {
+    this.huts = [];
+    array.forEach(huts, function(hut, index, array) {
+      var id = hut.id;
+      assert(id != null, "all given huts must have an id");
+      this.huts[id] = hut;  
+    }, this);
+  };
+
+  /**
+   * @private
+   */
+  hutmap.FilteredHuts.prototype._huts_changed = function() {
+    this._check_rep();
+    this.recompute_filtered_huts = true;
+    this.dispatchEvent(hutmap.FilteredHuts.EventType.HUTS_CHANGED);
   };
 
   /** Filters */
@@ -161,7 +197,7 @@ goog.scope(function() {
    * @return {Object.<string, hutmap.Filter>} the filters contained in this
    */
   hutmap.FilteredHuts.prototype.get_filters = function() {
-    throw "not implemented";
+    return object.createImmutableView(this.filters.toObject());
   };
 
   /**
@@ -174,14 +210,27 @@ goog.scope(function() {
    * @param {hutmap.Filter} filter the filter
    */
   hutmap.FilteredHuts.prototype.set_filter = function(field, filter) {
-    throw "not implemented";
+    assertString(field);
+    assertInstanceof(filter, hutmap.Filter);
+    this.filters.set(field, filter);
+    this._filters_changed();
   };
 
   /**
    * Clear all filters from this.
    */
   hutmap.FilteredHuts.prototype.clear_filters = function() {
-    throw "not implemented";
+    this.filters.clear();
+    this._filters_changed();
+  };
+
+  /**
+   * @private
+   */
+  hutmap.FilteredHuts.prototype._filters_changed = function() {
+    this._check_rep(); 
+    this.recompute_filtered_huts = true;
+    this.dispatchEvent(hutmap.FilteredHuts.EventType.FILTERS_CHANGED);
   };
 });
 
