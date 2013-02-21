ed9431403033078cced7cd03257c61b4f805aea0
Dylan Price <the.dylan.price@gmail.com>
2013-02-20 17:06:39 -0800

    Moved constructor argument checking into its own function. Added _check_rep method and currently check that this.id is a number >= 0.
    --HG--
    branch : js-redesign

--- scripts/make/generate/templates/models.js
+++ scripts/make/generate/templates/models.js
@@ -16,8 +16,9 @@ Context:
 {% for model in models %}
 goog.provide('hutmap.{{ model.name }}');
 {% endfor %}
-goog.require('goog.structs.Map');
+goog.requier('goog.asserts');
 goog.require('goog.array');
+goog.require('goog.structs.Map');
 
 {% for model in models %}
 hutmap.{{ model.name }} = function(values) {
@@ -25,25 +26,7 @@ hutmap.{{ model.name }} = function(values) {
     values = {};
 
   if (goog.DEBUG) {
-    var map = new goog.structs.Map(values);
-    keys = map.getKeys();
-    fields = [
-      {# put all methods added to the models here #}
-      'equals',
-      {% for field in model.fields %}
-      '{{ field }}',
-      {% if not forloop.last %}
-      '{{ field }}_display',
-      {% else %}
-      '{{ field }}_display'
-      {% endif %}
-      {% endfor %}
-    ];
-    goog.array.forEach(keys, function(key, index, array) {
-      if (!goog.array.contains(fields, key)) {
-        throw "hutmap.{{ model.name }}(): Invalid field '" + key + "' given in values parameter.";
-      }
-    });
+    this._check_ctor_args(values);
   }
 
   {% for field in model.fields %}
@@ -73,6 +56,39 @@ hutmap.{{ model.name }} = function(values) {
     {% endfor %}
   }
   {% endfor %}
+
+  if (goog.DEBUG) {
+    this._check_rep();
+  }
+};
+
+hutmap.{{ model.name }}.prototype._check_ctor_args = function(values) {
+  var map = new goog.structs.Map(values);
+  keys = map.getKeys();
+  fields = [
+    {# put all methods added to the models here #}
+    'equals',
+    {% for field in model.fields %}
+    '{{ field }}',
+    {% if not forloop.last %}
+    '{{ field }}_display',
+    {% else %}
+    '{{ field }}_display'
+    {% endif %}
+    {% endfor %}
+  ];
+  goog.array.forEach(keys, function(key, index, array) {
+    if (!goog.array.contains(fields, key)) {
+      throw "hutmap.{{ model.name }}(): Invalid field '" + key + "' given in values parameter.";
+    }
+  });
+};
+
+hutmap.{{ model.name }}.prototype._check_rep = function() {
+  {% if 'id' in model.fields %}
+  goog.asserts.assertNumber(this.id);
+  goog.asserts.assert(this.id >= 0);
+  {% endif %}
 };
 
 hutmap.{{ model.name }}.prototype.equals = function(other) {
