1c3d328cf81b83ba2142abfc83c3abadca0a4260
Dylan Price <the.dylan.price@gmail.com>
2013-02-21 14:21:17 -0800

    Reordered variables.
    --HG--
    branch : js-redesign

--- scripts/make/config.py
+++ scripts/make/config.py
@@ -1,14 +1,14 @@
 from os.path import join, dirname, normpath
 import os
 
+LOCAL_PATH = normpath(join(dirname(__file__), '.'))
+BASE_PATH = join(LOCAL_PATH, '..', '..')
+SRC_PATH = join(BASE_PATH, 'src')
+
 CLOSURE_LIBRARY = os.environ['HUTMAP_CLOSURE_LIBRARY']
 CLOSURE_COMPILER = os.environ['HUTMAP_CLOSURE_COMPILER']
 CLOSURE_TEMPLATES = os.environ['HUTMAP_CLOSURE_TEMPLATES']
 DEV_PATH = os.environ['HUTMAP_DEV_PATH']
-
-LOCAL_PATH = normpath(join(dirname(__file__), '.'))
-BASE_PATH = join(LOCAL_PATH, '..', '..')
-SRC_PATH = join(BASE_PATH, 'src')
 
 CSS_PATH = join(SRC_PATH, 'css')
 HUTMAP_PATH = join(SRC_PATH, 'hutmap')
