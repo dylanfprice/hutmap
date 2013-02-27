67122b38777e3fefec1eecc8564f14723b3426fc
Dylan Price <the.dylan.price@gmail.com>
2013-02-26 23:41:26 -0800

    Configuration for testacular.
    --HG--
    branch : js-redesign

---
+++ src/js-test/hutmap/config/testacular.conf.js
@@ -0,0 +1,20 @@
+basePath = '../';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  '../../../deps/angular-seed/app/lib/angular/angular.js',
+  '../../../deps/angular-seed/app/lib/angular/angular-*.js',
+  '../lib/angular/angular-mocks.js',
+  '../../js/hutmap/**/*.js',
+  'unit/**/*.js'
+];
+
+autoWatch = true;
+
+browsers = ['Firefox'];
+
+junitReporter = {
+  outputFile: 'test_out/unit.xml',
+  suite: 'unit'
+};
