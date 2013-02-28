52683186bbd32af64172fdda992c191515f01055
Dylan Price <the.dylan.price@gmail.com>
2013-02-28 01:41:28 -0800

    Moved to simpler config system.
    --HG--
    branch : js-redesign

--- src/js/google-maps/module.js
+++ src/js/google-maps/module.js
@@ -14,57 +14,14 @@
   /**
    * Default configuration.
    */
-  constant('googleMapsConfigDefaults', {
-    maps: {
-      'default': {
-        mapOptions: {
-          zoom : 8,
-          center : new google.maps.LatLng(46.87916, -120),
-          mapTypeId : google.maps.MapTypeId.ROADMAP
-        },
-        markerTypes: {
-          'default': {
-            markerOptions: {
-            }
-          }
-        },
-        selectedMarker: {
-          markerOptions: {
-          } 
-        }
-      }
+  value('googleMapsDefaults', {
+    'mapOptions': {
+      zoom : 8,
+      center : new google.maps.LatLng(46.87916, -120),
+      mapTypeId : google.maps.MapTypeId.ROADMAP
     },
-  }).
-
-  /**
-   * Override this service in your app to provide configuration for the map,
-   * markers, etc.
-   *
-   * Format is as follows, refer to the googleMapsConfigDefaults constant
-   * service for an example.
-   *
-   * maps: {
-   *   'mapId1': {
-   *     mapOptions: {
-   *       // google.maps.MapOptions here
-   *     },
-   *     markerTypes: {
-   *       'type1': {
-   *         markerOptions: {
-   *           // google.maps.MarkerOptions here
-   *         }
-   *       },
-   *       // more marker types may go here...
-   *     },
-   *     selectedMarker: {
-   *       markerOptions: {
-   *         // google.maps.MarkerOptions here
-   *       }
-   *     }
-   *   },
-   *   // more map ids may go here...
-   * }
-   */
-  value('googleMapsConfig', {});
+    'markerOptions': {
+    }
+  });
 
 })();
