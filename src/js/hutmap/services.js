efbdfc830971d453449a9b6af0e6700929d2e5ba
Dylan Price <the.dylan.price@gmail.com>
2013-03-08 16:56:14 -0800

    Added simple  to use hutmap api.
    --HG--
    branch : js-redesign

--- src/js/hutmap/services.js
+++ src/js/hutmap/services.js
@@ -1,5 +1,11 @@
 'use strict';
 
 (function () {
-  angular.module('hutmap.services', []);
+  angular.module('hutmapServices', []).
+
+  factory('Huts', ['$resource', function($resource){
+    return $resource('/huts/api/v1/hut', {}, {
+      query: {method:'GET', params:{limit: 0}
+    }});
+  }]);
 })();
