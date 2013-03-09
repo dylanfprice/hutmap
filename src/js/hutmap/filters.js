f97c3d07c3c1c35c6ae8074132ef05f4ceec39fb
Dylan Price <the.dylan.price@gmail.com>
2013-03-08 16:56:30 -0800

    Added truncate filter.
    --HG--
    branch : js-redesign

--- src/js/hutmap/filters.js
+++ src/js/hutmap/filters.js
@@ -1,12 +1,32 @@
 'use strict';
 
 (function () {
-  var app = angular.module('hutmap.filters', []);
+  angular.module('hutmapFilters', []).
 
-  app.filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+  /**
+  * Truncate Filter
+  * @Param string
+  * @Param int, default = 10
+  * @Param string, default = "..."
+  * @return string
+  */
+  filter('truncate', function () {
+    return function (text, length, end) {
+      if (isNaN(length))
+        length = 10;
+         
+      if (end === undefined)
+        end = "...";
+         
+      if (text == null || text.length == 0) {
+        return null;
+      } else if (text.length <= length || text.length - end.length <= length) {
+        return text;
+      }
+      else {
+        return String(text).substring(0, length-end.length) + end;
+      }
+    };
+  });
 
 })();
