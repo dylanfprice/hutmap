f6a0e675897ffe6fef2b3e40189bee04abd894a3
Dylan Price <the.dylan.price@gmail.com>
2013-03-13 15:16:05 -0700

    Switched to new django url path.

--- src/hutmap/urls.py
+++ src/hutmap/urls.py
@@ -1,16 +1,18 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
+from django.conf import settings
 
 from django.contrib.gis import admin
 admin.autodiscover()
 
 urlpatterns = patterns('django.views.generic.simple',
   url(r'^$',  'direct_to_template', {'template': 'index.html'}, name='hutmap_home'),
+  url(r'^test$',  'direct_to_template', {'template': 'test.html'}),
 )
 
 urlpatterns += patterns('',
   url(r'^huts/',       include('huts.urls')),
-  #url(r'^blog/',       include('blog.urls'), name='hutmap_blog'),
-  #url(r'^about/',      include('blog.urls'), name='hutmap_about'),
+  #url(r'^browse/',    include('blog.urls'), name='hutmap_browse'),
+  #url(r'^about/',     include('blog.urls'), name='hutmap_about'),
   url(r'^admin/',      include(admin.site.urls)),
   url(r'^admin/doc/',  include('django.contrib.admindocs.urls')),
 )
