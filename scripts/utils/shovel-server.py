42c3c7f3252da7bf6677cdac0bb6f2601aa37f36
Dylan Price <the.dylan.price@gmail.com>
2013-03-12 22:10:58 -0700

    Script to start shovel-server on Vagrant vm.
    --HG--
    branch : js-redesign

---
+++ scripts/utils/shovel-server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# Start shovel-server on Vagrant vm
+
+from os.path import join, dirname, normpath
+import os
+import subprocess
+import sys
+
+base = normpath(join(dirname(__file__), '..'))
+os.chdir(base)
+
+def help_text(retcode):
+  print("Usage: shovel-server.py start|stop")
+  sys.exit(retcode)
+
+if len(sys.argv) < 2:
+  help_text(1)
+
+try:
+  command = sys.argv[1]
+except:
+  help_text(1)
+
+if command == '-h' or command == '--help' or command == 'help':
+  help_text(0)
+elif command == 'start':
+  subprocess.check_call(['vagrant', 'ssh', '-c', 'cd /vagrant/scripts && echo "shovel-server --port 3000" | at now'])
+  print('Shovel server started. Try going to http://localhost:3000/help in your browser.')
+elif command == 'stop':
+  subprocess.check_call(['vagrant', 'ssh', '-c', 'killall shovel-server'])
+  print('Shovel server stopped.')
+else:
+  help_text(1)
+
