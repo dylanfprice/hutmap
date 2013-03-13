f92582bd80d00da06843f34e1005c35e48237c15
Dylan Price <the.dylan.price@gmail.com>
2013-03-12 22:33:59 -0700

    Better output.
    --HG--
    branch : js-redesign

--- scripts/shovel/test.py
+++ scripts/shovel/test.py
@@ -6,7 +6,6 @@ import subprocess
 file = os.path.normpath(os.path.join(os.path.dirname(__file__), 'config.py'))
 config = imp.load_source('config', file)
 
-
 @task
 def hutmapjs():
   """Tests hutmap javascript application"""
@@ -23,7 +22,16 @@ def hutmapjs():
 @task
 def gmapsjs():
   """Tests google-maps angular module"""
-  subprocess.check_call([
+  proc = subprocess.Popen([
     'testacular', 'start',
     '{0}/google-maps/config/testacular.conf.js'.format(config.JS_TEST_PATH),
-    '--single-run'])
+    '--single-run'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+  stdoutdata,stderrdata = proc.communicate()
+  retcode = proc.returncode
+
+  if retcode == 0:
+    print('All tests passed!')
+  else:
+    print('TEST FAIL:')
+    print(stdoutdata)
