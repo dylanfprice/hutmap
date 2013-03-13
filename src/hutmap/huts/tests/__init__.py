bfa2ad02335490d1501192f61328f614194fe5d3
Dylan Price <the.dylan.price@gmail.com>
2013-03-13 16:30:36 -0700

    Change unittest2 to unittest.

--- src/hutmap/huts/tests/__init__.py
+++ src/hutmap/huts/tests/__init__.py
@@ -1,9 +1,9 @@
-import unittest2
+import unittest
 from django.conf import settings
 from os.path import join, dirname, normpath
 
 def suite():
-  return unittest2.TestLoader().discover(
+  return unittest.TestLoader().discover(
     normpath(join(dirname(__file__), '.')),
     pattern='*.py',
     top_level_dir=settings.LOCAL_PATH
