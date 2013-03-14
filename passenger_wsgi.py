e417b175d0b0fba06cdad8c6c786a76b66424384
Dylan Price <the.dylan.price@gmail.com>
2013-03-14 15:15:36 -0700

    Switched form logging to print to simple file.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,20 +1,19 @@
-import logging
 import os
 import sys 
 
 cwd = os.getcwd()
 
-# configure logging
-logfilename = os.path.join(cwd, 'passenger_wsgi.log')
-logging.basicConfig(filename=logfilename, level=logging.WARNING)
-logging.info("Running %s", __file__)
-logging.info("environ: %s", str(os.environ))
+# configure 'logging'
+logfilename = os.path.join(cwd, 'logs', 'passenger_wsgi.log')
+logfile = open(logfilename, 'w')
+logfile.write("Running {0}\n".format(__file__))
+logfile.write("environ: {0}\n".format(str(os.environ)))
 
 
 # run in our virtualenv
 INTERP = os.path.join(os.environ['HOME'], '.pythonbrew', 'venvs', 'Python-2.7.3', 'hutmap', 'bin', 'python')
 if sys.executable != INTERP:
-  logging.info("sys.executable ({0}) was not {1}. Re-executing.".format(sys.executable, INTERP))
+  logfile.write("sys.executable ({0}) was not {1}. Re-executing.\n".format(sys.executable, INTERP))
   os.execl(INTERP, INTERP, *sys.argv)
 
 
@@ -25,7 +24,7 @@ try:
   sys.path.insert(0, src_dir)
   sys.path.insert(0, hutmap_dir)
 except Exception as e:
-  logging.exception(e) 
+  logfile.write("ERROR: {0}\n".format(e))
   raise e
 
 # start django
@@ -33,7 +32,9 @@ try:
   from django.core.wsgi import get_wsgi_application
   os.environ['DJANGO_SETTINGS_MODULE'] = "hutmap.settings"
   application = get_wsgi_application()
-  logging.info("application executed successfully")
+  logfile.write("application executed successfully\n")
 except Exception as e:
-  logging.exception("Error: %s", e)
+  logfile.write("ERROR: {0}\n".format(e))
   raise e
+
+logfile.close()
