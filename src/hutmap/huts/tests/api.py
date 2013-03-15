84a49b8c36bd87738400e6bad1803ed3cdf98798
Dylan Price <the.dylan.price@gmail.com>
2013-03-14 19:10:10 -0700

    Added hack to ensure 'distance' function in mysql.

--- src/hutmap/huts/tests/api.py
+++ src/hutmap/huts/tests/api.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.test import TestCase
 import json
+import subprocess
 
 def float_equals(a, b):
   return abs(a - b) < 0.00001
@@ -7,6 +9,16 @@ def float_equals(a, b):
 class HutResourceTestCase(TestCase):
   fixtures = ['test_data.json']
   url = '/huts/api/v1/hut/'
+
+  @classmethod
+  def setUpClass(cls):
+    # TODO: this is a hack
+    # Create the 'distance' function in mysql
+    cmd = ["mysql -u {user} -p'{passw}' < /etc/mysql/hutmap_fns.sql".format( 
+             user=settings.DATABASES['default']['USER'],
+             passw=settings.DATABASES['default']['PASSWORD'])]
+    subprocess.check_call(cmd, shell=True)
+    print("Added 'distance' function to mysql")
 
   def test_query_bbox(self):
     response = self.client.get(self.url + '?bbox=47.05814,-122.79683,49.05814,-120.79683')
