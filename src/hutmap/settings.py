5c36544a043376293e71aec97bb193df23535934
Dylan Price <the.dylan.price@gmail.com>
2013-03-15 14:16:42 -0700

    Added conditional get middleware.'

--- src/hutmap/settings.py
+++ src/hutmap/settings.py
@@ -152,6 +152,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
