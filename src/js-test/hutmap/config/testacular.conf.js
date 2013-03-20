a2688ad168d5e571cd8a1b457d3d7abb0c569c0f
Dylan Price <the.dylan.price@gmail.com>
2013-03-19 21:21:42 -0700

    Updated testacular config.
    --HG--
    branch : permalinks

--- src/js-test/hutmap/config/testacular.conf.js
+++ src/js-test/hutmap/config/testacular.conf.js
@@ -3,15 +3,17 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
+  'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry&key=AIzaSyDKzNYkeusCcplMH7XWA9l0QLZ8Cwn01_M',
+  'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
+  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js',
   '../lib/angular/angular-mocks.js',
+  '../../js/third-party/**/*.js',
+  '../../js/google-maps/module.js',
+  '../../js/google-maps/**/*.js',
   '../../js/hutmap/app.js',
   '../../js/hutmap/**/*.js',
   'unit/**/*.js'
-];
-
-exclude = [
-  '../../js/hutmap/config.js' // references google.maps
 ];
 
 autoWatch = true;
