23b5a3e450582bcfe1ac96d7c62c261ef25cddcf
Dylan Price <the.dylan.price@gmail.com>
2013-03-20 00:02:36 -0700

    Updated test config.

--- src/js-test/hutmap/config/testacular.conf.js
+++ src/js-test/hutmap/config/testacular.conf.js
@@ -3,10 +3,12 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry&key=AIzaSyDKzNYkeusCcplMH7XWA9l0QLZ8Cwn01_M',
+  'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry&key=AIzaSyDKzNYkeusCcplMH7XWA9l0QLZ8Cwn01_M',
   'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js',
+  'http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/bootstrap.min.js',
+  'http://cdnjs.cloudflare.com/ajax/libs/angular-strap/0.6.6/angular-strap.min.js',
   '../lib/angular/angular-mocks.js',
   '../../js/third-party/**/*.js',
   '../../js/google-maps/module.js',
