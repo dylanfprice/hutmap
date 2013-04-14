85ac3c9f16b5bd7293053e728026d69abc46821e
Dylan Price <the.dylan.price@gmail.com>
2013-04-13 20:16:25 -0700

    Updated test config.

--- src/js-test/hutmap/config/karma.conf.js
+++ src/js-test/hutmap/config/karma.conf.js
@@ -1,24 +1,31 @@
-basePath = '../';
+basePath = '../../../';
 
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry&key=AIzaSyDKzNYkeusCcplMH7XWA9l0QLZ8Cwn01_M',
-  'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+  'https://maps.googleapis.com/maps/api/js?sensor=false',
+  'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js',
-  '../lib/angular/angular-mocks.js',
-  '../../js/third-party/**/*.js',
-  '../../js/hutmap/app.js',
-  '../../js/hutmap/**/*.js',
-  'unit/**/*.js'
+  'js-test/lib/angular/angular-mocks.js',
+  'js/third-party/**/*.js',
+  'js/hutmap/app.js',
+  'js/hutmap/**/*.js',
+  'js-test/hutmap/unit/**/*.js'
 ];
 
 autoWatch = true;
 
 browsers = ['PhantomJS'];
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+preprocessors = {
+  '**/src/**/*.js': 'coverage'
 };
+
+reporters = ['progress', 'coverage'];
+
+coverageReporter = {
+  type : 'html',
+  dir : 'js-test/hutmap/coverage/'
+};
+
