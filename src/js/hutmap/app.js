4bec449c3a97c973476cbe4915c9155516283fbf
Dylan Price <the.dylan.price@gmail.com>
2013-04-22 18:48:47 -0700

    Moved config into app.js.

--- src/js/hutmap/app.js
+++ src/js/hutmap/app.js
@@ -2,5 +2,61 @@
 
 (function() {
   angular.module('hutmap', ['hutmapServices', 'hutmapFilters', 'AngularGM',
-    'ngResource', 'ui.bootstrap']);
+    'ngResource', 'ui.bootstrap']).
+
+  config(['$routeProvider', '$locationProvider', 'PlacesProvider', function($routeProvider, $locationProvider, PlacesProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/partials/home.html',
+        activetab: 'home',
+      }).
+      when('/map/', {
+        templateUrl: '/partials/map.html',
+        activetab: 'map',
+        reloadOnSearch: false,
+      }).
+      when('/about/', {
+        templateUrl: '/partials/about.html',
+        activetab: 'about',
+      });
+
+    $locationProvider.html5Mode(true);
+
+    PlacesProvider.bounds(new google.maps.LatLngBounds(
+        new google.maps.LatLng(30, -130),
+        new google.maps.LatLng(65, -80)));
+  }]).
+
+  constant('hutmapMapId', 'map_canvas').
+
+  value('mapOptions', {
+    zoom : 8,
+    center : new google.maps.LatLng(46.87916, -120),
+    mapTypeId : google.maps.MapTypeId.TERRAIN,
+    streetViewControl: false
+  }).
+
+  value('markerOptions', {
+    huts: {
+      icon: '/static/img/marker_gray_small.png',
+      shape: {
+        coord: [6, 0, 2, 2, 0, 6, 6, 13, 12, 6, 10, 2, 6, 0],
+        type: 'poly'
+      },
+      zIndex: 0
+    },
+    filteredHuts: {
+      icon: '/static/img/marker_red_small.png',
+      shape: {
+        coord: [6, 0, 2, 2, 0, 6, 6, 13, 12, 6, 10, 2, 6, 0],
+        type: 'poly'
+      },
+      zIndex: 1
+    },
+    selected: {
+      icon: '/static/img/marker_yellow_small.png',
+      zIndex: 2
+    }
+  });
+
 })();
