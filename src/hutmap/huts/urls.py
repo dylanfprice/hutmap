e7ec0f98cdabcd8cac4c45f781b5b0a0b7cccd65
Dylan Price <the.dylan.price@gmail.com>
2013-04-22 23:39:26 -0700

    Only expose hutsearch in api.

--- src/hutmap/huts/urls.py
+++ src/hutmap/huts/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import patterns, include
+from huts.api import HutSearchResource
 from tastypie.api import Api
 
-from huts.api import HutResource, HutSearchResource, RegionResource, AgencyResource
-
 api = Api(api_name='v1')
-api.register(HutResource())
 api.register(HutSearchResource())
-api.register(RegionResource())
-api.register(AgencyResource())
-#api.register(HutTypeResource())
 
 urlpatterns = patterns('',
   (r'^api/', include(api.urls)),
