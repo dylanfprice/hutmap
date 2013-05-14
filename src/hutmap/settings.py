0e73220b0deba7ae897c5568abebd450e7360a66
Dylan Price <the.dylan.price@gmail.com>
2013-05-14 16:31:26 -0700

    Trying staticfiles_dirs

--- src/hutmap/settings.py
+++ src/hutmap/settings.py
@@ -115,7 +115,8 @@ ADMIN_MEDIA_PREFIX = '/admin_static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/app/static/'
+STATICFILES_DIRS = [join(LOCAL_PATH, 'public', 'static')]
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
