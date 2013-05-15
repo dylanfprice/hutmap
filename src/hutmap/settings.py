2d62a3beb074b295ed8ed4d2867c51132a822538
Dylan Price <the.dylan.price@gmail.com>
2013-05-14 17:38:41 -0700

    Fix static path.

--- src/hutmap/settings.py
+++ src/hutmap/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(LOCAL_PATH, 'public', 'static'),
+    join(LOCAL_PATH, '..', '..', 'public', 'static'),
 )
 
 # List of finder classes that know how to find static files in
