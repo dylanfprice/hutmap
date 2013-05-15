af7babd2ab1c6e2330c451b88c0ff0b9e0761417
Dylan Price <the.dylan.price@gmail.com>
2013-05-14 17:58:08 -0700

    Fixed static directory.

--- src/hutmap/settings.py
+++ src/hutmap/settings.py
@@ -115,7 +115,7 @@ ADMIN_MEDIA_PREFIX = '/admin_static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = join(LOCAL_PATH, '..', '..', 'public')
+STATIC_ROOT = join(LOCAL_PATH, '..', '..', 'public', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
