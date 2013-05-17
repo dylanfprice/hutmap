0a499999782df49f8dffbd92335245f3510e824c
Dylan Price <the.dylan.price@gmail.com>
2013-05-16 17:09:44 -0700

    First pass at deploy scripts to dreamhost.

---
+++ scripts/utils/deploy-dreamhost.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from os.path import join, dirname, normpath
+import os
+import shlex
+import subprocess
+import urllib2
+import uuid
+
+def shell(cmd):
+  args = shlex.split(cmd) 
+  subprocess.check_call(args)
+
+
+base_path = normpath(join(dirname(__file__), '..', '..'))
+os.chdir(base_path)
+
+vers = uuid.uuid1()
+
+shell('git checkout dreamhost')
+shell('git merge -s resolve master')
+shell('git rm -r public/static/css/ public/static/js/')
+shell('python scripts/utils/shovel-server.py start')
+
+urllib2.open('http://localhost:3000/build.css?version={0}'.format(vers))
+urllib2.open('http://localhost:3000/build.js?version={0}'.format(vers))
+
+shell('git add public/static/css/ public/static/js/')
+shell('git ci -m"Version {0}"'.format(vers))
+#shell('git push origin master')
+#shell('ssh hutmap@hutmap.com "bash -s {0}" < scripts/utils/deploy-dreamhost-remote.sh'.format(vers))
