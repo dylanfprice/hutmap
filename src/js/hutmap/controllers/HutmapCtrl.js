ef8095b3db3ed4b79f298f11b694fcbdcf10aeec
Dylan Price <the.dylan.price@gmail.com>
2013-08-06 19:40:56 -0700

    Broadcast gmMapResize when map is resized.

--- src/js/hutmap/controllers/HutmapCtrl.js
+++ src/js/hutmap/controllers/HutmapCtrl.js
@@ -5,7 +5,7 @@
 
   controller('HutmapCtrl', 
     ['$scope', '$route', '$timeout', 
-    function($scope, $route, $timeout) {
+    function($scope, $route, $timeout, hutmapMapId) {
 
     $scope.$route = $route;
 
@@ -18,7 +18,7 @@
       // make sure the timeout is greater than the css transition time, see
       // map.less
       $timeout(function() {
-        $scope.$broadcast('gmapResized');
+        $scope.$broadcast('gmMapResize', hutmapMapId);
       }, 600);
     });
 
