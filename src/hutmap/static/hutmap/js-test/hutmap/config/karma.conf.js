41c7b36b006ee7d729063ef8f9d98d02668a27d7
Dylan Price <the.dylan.price@gmail.com>
2013-08-23 22:29:14 -0700

    Updated karma config to latest version and install karma-coverage plugin.

--- src/hutmap/static/hutmap/js-test/hutmap/config/karma.conf.js
+++ src/hutmap/static/hutmap/js-test/hutmap/config/karma.conf.js
@@ -1,35 +1,38 @@
-basePath = '../../../';
+module.exports = function(config) {
+  config.set({
+    basePath: '../../../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places',
-  'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
-  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js',
-  'js-test/lib/angular/angular-mocks.js',
-  'js/third-party/**/*.js',
-  'js/hutmap/app.js',
-  'js/hutmap/**/module.js',
-  'js/hutmap/directives.js',
-  'js/hutmap/filters.js',
-  'js/hutmap/**/*.js',
-  'js-test/hutmap/unit/fixtures.js',
-  'js-test/hutmap/unit/**/*.js'
-];
+    files: [
+      'https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places',
+      'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular-resource.min.js',
+      'js-test/lib/angular/angular-mocks.js',
+      'js/third-party/**/*.js',
+      'js/hutmap/app.js',
+      'js/hutmap/**/module.js',
+      'js/hutmap/directives.js',
+      'js/hutmap/filters.js',
+      'js/hutmap/**/*.js',
+      'js-test/hutmap/unit/fixtures.js',
+      'js-test/hutmap/unit/**/*.js'
+    ],
 
-autoWatch = true;
+    frameworks: ['jasmine'],
 
-browsers = ['PhantomJS'];
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    reportSlowerThan: 500,
 
-preprocessors = {
-  '**/src/**/*.js': 'coverage'
+    preprocessors: {
+      '**/src/**/*.js': 'coverage'
+    },
+
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'js-test/hutmap/coverage/'
+    }
+  });
 };
-
-reporters = ['progress', 'coverage'];
-
-coverageReporter = {
-  type : 'html',
-  dir : 'js-test/hutmap/coverage/'
-};
-
