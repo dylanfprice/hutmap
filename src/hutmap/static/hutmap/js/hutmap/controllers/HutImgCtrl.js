3b897dced391f5b7ae1a8aafb4100233795b3fbb
Dylan Price <the.dylan.price@gmail.com>
2013-09-26 23:14:31 -0700

    Use show_satellite field.

--- src/hutmap/static/hutmap/js/hutmap/controllers/HutImgCtrl.js
+++ src/hutmap/static/hutmap/js/hutmap/controllers/HutImgCtrl.js
@@ -13,7 +13,7 @@
         }
         else if (hut.photo_url) {
           url = hut.photo_url;
-        } else if (hut.location_accuracy === 3 || hut.location_accuracy === 5) {
+        } else if (hut.show_satellite) {
           url = 'http://maps.googleapis.com/maps/api/staticmap' + 
                 '?center=' + hut.location.coordinates[1] + '%2C' + hut.location.coordinates[0] +
                 '&zoom=19' +
