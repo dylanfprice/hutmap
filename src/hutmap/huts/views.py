efb5e7a22af6616ef54f948b8b27f55100a16fd8
Dylan Price <the.dylan.price@gmail.com>
2013-10-03 20:52:01 -0700

    Use urls_by_namespace function. Sweet!

--- src/hutmap/huts/views.py
+++ src/hutmap/huts/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from djangular.core.urlresolvers import urls_by_namespace
 from huts.models import Hut
 
 class DynamicTemplateView(TemplateView):
@@ -17,5 +18,6 @@ class BaseView(TemplateView):
     context = super(TemplateView, self).get_context_data(**kwargs)
     context['metadata'] = self.metadata
     context['absolute_uri'] = self.request.build_absolute_uri()
+    context['hut_urls'] = urls_by_namespace('huts')
     return context
 
