cdae7f9a8cb6a0590a2d4579baefaa0cdf45e284
Dylan Price <the.dylan.price@gmail.com>
2013-10-29 01:44:27 -0700

    Fix up view for hut edits and refactor common logic

--- src/hutmap/huts/views.py
+++ src/hutmap/huts/views.py
@@ -1,10 +1,12 @@
-import json
-from django.views.generic import TemplateView
-from django.http import HttpResponseBadRequest
-from django.views.generic.detail import DetailView
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
+from django.views.generic import TemplateView, FormView
 from djangular.core.urlresolvers import urls_by_namespace
 from huts.forms import HutSuggestionForm, HutEditForm
 from huts.models import Hut
+import logging
+
+logger = logging.getLogger(__name__)
 
 class DynamicTemplateView(TemplateView):
   folder = None
@@ -25,27 +27,24 @@ class BaseView(TemplateView):
     context['hut_urls'] = urls_by_namespace('huts')
     return context
 
-class HutSuggestionFormView(TemplateView):
+class HutCommonMixin(object):
   template_name = 'forms/hut.html'
 
-  def get_context_data(self, **kwargs):
-    context = super(TemplateView, self).get_context_data(**kwargs)
-    context.update(hut_form=HutSuggestionForm())
-    return context
+  def form_valid(self, form):
+    form.save()
+    return HttpResponse(status=201)
 
-class HutEditFormView(DetailView):
-  template_name = 'forms/hut.html'
-  model = Hut
+class HutSuggestionFormView(HutCommonMixin, FormView):
+  form_class = HutSuggestionForm
 
-  def get_context_data(self, **kwargs):
-    context = super(HutEditFormView, self).get_context_data(**kwargs)
-    hut = self.get_object()
-    context.update(hut_form=HutEditForm(instance=hut))
-    return context
+class HutEditFormView(HutCommonMixin, FormView):
+  form_class = HutEditForm
 
-#  def post(self, request, *args, **kwargs):
-#    if not request.is_ajax():
-#      return HttpResponseBadRequest('Expected an XMLHttpRequest')
-#    in_data = json.loads(request.body)
-#    bound_contact_form = CheckoutForm(data={'subject': in_data.get('subject')})
-#    # now validate 'bound_contact_form' and use it as in normal Django
+  def get_initial(self):
+    pk = self.kwargs['pk']  
+    hut = Hut.objects.get(pk=pk)
+    initial = model_to_dict(hut)
+    initial['hut'] = pk
+    return initial
+
+
