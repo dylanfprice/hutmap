18b33baae35a5580f09f67826cb41ff092c9ac13
Dylan Price <the.dylan.price@gmail.com>
2013-11-07 00:48:41 -0800

    Fixed bug in serializing FieldFile objects

--- src/hutmap/huts/templatetags/huts_extras.py
+++ src/hutmap/huts/templatetags/huts_extras.py
@@ -1,4 +1,5 @@
 from django.contrib.gis.geos import Point
+from django.db.models.fields.files import FieldFile
 from django import forms, template
 from json import JSONEncoder
 from huts.utils.display import point_display_value
@@ -12,20 +13,14 @@ def css_classes(form, field_name):
     return field.widget.attrs.get('class', '')
   return ''
 
-class DjangoFileFieldEncoder(JSONEncoder):
+class Encoder(JSONEncoder):
   def default(self, o):
-    if isinstance(o, forms.FileField):
+    if isinstance(o, (forms.FileField, FieldFile)):
       return o.url
-    return super(DjangoFileFieldEncoder, self).default(o)
-
-class PointEncoder(JSONEncoder):
-  def default(self, o):
-    if isinstance(o, Point):
+    elif isinstance(o, Point):
       return point_display_value(o)
-    return super(DjangoFileFieldEncoder, self).default(o)
+    return super(Encoder, self).default(o)
 
-class Encoder(DjangoFileFieldEncoder, PointEncoder):
-  pass
 encoder = Encoder()
 
 @register.filter
