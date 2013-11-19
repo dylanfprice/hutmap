508640cccfd6ad7460ca8d29d35596aa31924c59
Dylan Price <the.dylan.price@gmail.com>
2013-11-18 19:42:06 -0800

    Added label models to api and fixed up export script

--- src/hutmap/huts/api.py
+++ src/hutmap/huts/api.py
@@ -1,9 +1,7 @@
-from django.contrib.gis.geos import Polygon, Point, MultiPolygon
-from django.utils.datastructures import SortedDict
-from huts.models import Hut, Region, Agency
+from huts.models import Hut, Region, Agency, Designation, System, AccessType,\
+                        HutType, Service
 from huts.utils.csv_serializer import CSVSerializer
 from huts.utils.csv_consts import CSV_NULL, CSV_TRUE, CSV_FALSE
-from tastypie.cache import SimpleCache
 from tastypie.contrib.gis.resources import ModelResource
 from tastypie import fields
 
@@ -12,6 +10,33 @@ class RegionResource(ModelResource):
     queryset = Region.objects.all()
     allowed_methods = ['get']
     serializer = CSVSerializer(formats=['json', 'csv'])
+
+class LabelResourceMixin(object):
+  class Meta:
+    allowed_methods = ['get']
+    serializer = CSVSerializer(formats=['json', 'csv'])
+    fields = ['identifier']
+    include_resource_uri = False
+
+class DesignationResource(ModelResource, LabelResourceMixin):
+  class Meta(LabelResourceMixin.Meta):
+    queryset = Designation.objects.all()
+
+class SystemResource(ModelResource, LabelResourceMixin):
+  class Meta(LabelResourceMixin.Meta):
+    queryset = System.objects.all()
+
+class AccessTypeResource(ModelResource, LabelResourceMixin):
+  class Meta(LabelResourceMixin.Meta):
+    queryset = AccessType.objects.all()
+
+class HutTypeResource(ModelResource, LabelResourceMixin):
+  class Meta(LabelResourceMixin.Meta):
+    queryset = HutType.objects.all()
+
+class ServiceResource(ModelResource, LabelResourceMixin):
+  class Meta(LabelResourceMixin.Meta):
+    queryset = Service.objects.all()
 
 class AgencyResource(ModelResource):
   parent = fields.ForeignKey('AgencyResource', 'agency', full=False, null=True)
@@ -27,19 +52,20 @@ class HutResource(ModelResource):
   agency = fields.ForeignKey(AgencyResource, 'agency', full=False, null=True)
   # list fields
   location_references = fields.ListField(attribute='location_references', null=True)
-  designations = fields.ListField(attribute='designations', null=True)
-  systems = fields.ListField(attribute='systems', null=True)
   alternate_names = fields.ListField(attribute='alternate_names', null=True)
-  access_no_snow = fields.ListField(attribute='access_no_snow', null=True)
-  types = fields.ListField(attribute='types', null=True)
-  services = fields.ListField(attribute='services', null=True)
+  # m2m fields
+  designations = fields.ToManyField(DesignationResource, 'designations', null=True, full=True)
+  systems = fields.ToManyField(SystemResource, 'systems', null=True, full=True)
+  access_no_snow = fields.ToManyField(AccessTypeResource, 'access_no_snow', null=True, full=True)
+  types = fields.ToManyField(HutTypeResource, 'types', null=True, full=True)
+  services = fields.ToManyField(ServiceResource, 'services', null=True, full=True)
 
   class Meta:
     max_limit = 0
     queryset = Hut.objects.published()
     list_allowed_methods = ['get']
     detail_allowed_methods = ['get']
-    #excludes = ['created', 'updated']
+    excludes = ['created', 'updated']
     serializer = CSVSerializer(formats=['json', 'csv'])
     filtering = {
       'name': ['startswith'],
--- src/hutmap/huts/utils/export.py
+++ src/hutmap/huts/utils/export.py
@@ -1,27 +1,34 @@
-from django.core.serializers import json
+from django.test import RequestFactory
 from huts.api import HutResource, AgencyResource, RegionResource
 from huts.models import Hut, Agency, Region
 
+request_factory = RequestFactory()
 
-def _get_objects_data(objects, resource):
+def _serialize_queryset(queryset, resource):
   objects_data = {
-    'meta': { 'total_count': len(objects)},
+    'meta': { 'total_count': len(queryset)},
     'object_index': {},
   }
-  for object in objects:
-    bundle = resource.build_bundle(obj=object)
+  for obj in queryset:
+    # have to make a fake request so tastypie doesn't blow up on m2m stuff
+    bundle = resource.build_bundle(obj=obj, request=request_factory.get('/'))
     bundle = resource.full_dehydrate(bundle)
     objects_data['object_index'][bundle.data['id']] = bundle.data
   
-  return objects_data
-
+  return resource.serialize(None, objects_data, 'application/json')
 
 def db_as_json():
-  data = {
-    'huts': _get_objects_data(Hut.objects.published(), HutResource()),
-    'agencies': _get_objects_data(Agency.objects.all(), AgencyResource()),
-    'regions': _get_objects_data(Region.objects.all(), RegionResource()),
-  }
+  json_template = u'''
+    {{
+      "huts": {},
+      "agencies": {},
+      "regions": {}
+    }}
+  '''
 
-  return json.json.dumps(data, cls=json.DjangoJSONEncoder, sort_keys=True,
-      ensure_ascii=False)
+  return json_template.format(
+      _serialize_queryset(Hut.objects.published(), HutResource()),
+      _serialize_queryset(Agency.objects.all(), AgencyResource()),
+      _serialize_queryset(Region.objects.all(), RegionResource()),
+  )
+  
