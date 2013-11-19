5268cdddaf20a6118dd3a69d53ca4a40800ba9d0
Dylan Price <the.dylan.price@gmail.com>
2013-11-19 00:10:20 -0800

    Removed unused blur directive.

--- src/hutmap/static/hutmap/js/hutmap/directives.js
+++ src/hutmap/static/hutmap/js/hutmap/directives.js
@@ -1,16 +1,7 @@
 (function () {
 'use strict';
 
-  angular.module('hutmap.directives', [])
-
-  .directive('blur', ['$parse', function ($parse) {
-    return function (scope, elem, attrs) {
-      var blurFn = $parse(attrs.blur);
-      elem.bind('blur', function () {
-        blurFn(scope);
-      });
-    };
-  }]).
+  angular.module('hutmap.directives', []).
 
   directive('hutForm', ['$compile', function ($compile) {
     return {
