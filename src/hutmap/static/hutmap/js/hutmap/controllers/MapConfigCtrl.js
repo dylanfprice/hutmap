762edf0ce49afd3bd364de3a09533ae160ed333b
Dylan Price <the.dylan.price@gmail.com>
2014-04-22 00:52:55 -0700

    Revert "Don't unselect hut when map dragged."
    This reverts commit 300cceb8ef2c65f64e442944caeba82f70788c44.

--- src/hutmap/static/hutmap/js/hutmap/controllers/MapConfigCtrl.js
+++ src/hutmap/static/hutmap/js/hutmap/controllers/MapConfigCtrl.js
@@ -27,31 +27,19 @@
     // return proper google.maps.MapOptions for the given hut
     $scope.getMarkerOptions = function(hut) {
       var opts = {};
-
       if ($scope.h.filteredHuts && hut.id in $scope.h.filteredHutIds) {
-        opts = angular.extend(opts, markerOptions.filteredHuts);
+        return angular.extend(opts, markerOptions.filteredHuts);
       } else {
-        opts = angular.extend(opts, markerOptions.huts);
+        return angular.extend(opts, markerOptions.huts);
       }
-
-      if ($scope.h.selectedHut && $scope.h.selectedHut.id === hut.id) {
-        opts = angular.extend(opts, markerOptions.selected);
-      }
-
-      return opts;
     };
 
-    $scope.unselectHut = function() {
+    // called when marker is clicked or hut is otherwise selected
+    $scope.selectHut = function(marker, hut) {
       if (prevSelectedMarker) {
         var opts = angular.extend({}, markerOptions.huts, {icon: prevIcon});
         prevSelectedMarker.setOptions(opts);
       }
-      $scope.h.selectedHut = null;
-    }
-
-    // called when marker is clicked or hut is otherwise selected
-    $scope.selectHut = function(marker, hut) {
-      $scope.unselectHut();
       prevSelectedMarker = marker;
       prevIcon = marker.getIcon();
       marker.setOptions(markerOptions.selected);
