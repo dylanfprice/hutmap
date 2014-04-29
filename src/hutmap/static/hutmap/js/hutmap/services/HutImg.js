a3b7c286b9111d6aae7e187b974cd3781f7f8241
Dylan Price <the.dylan.price@gmail.com>
2014-04-29 01:00:35 -0700

    Refactored HutImgCtrl into a service.

---
+++ src/hutmap/static/hutmap/js/hutmap/services/HutImg.js
@@ -0,0 +1,86 @@
+(function () {
+'use strict';
+
+  angular.module('hutmap.services').
+
+  // get images for huts
+  provider('HutImg', [function() {
+    var HutImgProvider = {}
+    var defaultSatellite = {
+      width: 300,
+      height: 250,
+    };
+
+    HutImgProvider.$get = ['$q', 'MaxZoom', 'utils', function($q, MaxZoom, utils) {
+      var HutImg = {};
+
+      function getObliqueImgUrl(hut) {
+        var url = '';
+        if (hut) {
+          if (hut.photo) {
+            url = hut.photo;
+          }
+          else if (hut.photo_url) {
+            url = hut.photo_url;
+          } 
+        }
+        return url;
+      }
+
+      function getSatelliteImgUrl(hut, width, height) {
+        var urlDeferred = $q.defer();
+        var latLng = utils.latLngFromHut(hut);
+        var maxZoomPromise = MaxZoom.getMaxZoom(latLng);
+        maxZoomPromise.then(function(maxZoom) {
+          var url = 'http://maps.googleapis.com/maps/api/staticmap' + 
+                    '?center=' + latLng.lat() + '%2C' + latLng.lng() +
+                    '&zoom=' + maxZoom +
+                    '&size=' + width + 'x' + height +
+                    '&maptype=satellite' +
+                    '&sensor=false' +
+                    '&key=' + hutmap.GOOGLE_API_KEY;
+          urlDeferred.resolve(url);
+        });
+        return urlDeferred.promise;
+      };
+
+      HutImg.getHutImgUrl = function(hut) {
+        var urlDeferred = $q.defer();
+        if (hut && hut.show_satellite) {
+          var urlPromise = getSatelliteImgUrl(hut, defaultSatellite.width, defaultSatellite.height);
+          urlPromise.then(function(satelliteUrl) {
+            urlDeferred.resolve(satelliteUrl);
+          });
+        } else {
+          var obliqueUrl = getObliqueImgUrl(hut);
+          if (obliqueUrl) {
+            urlDeferred.resolve(obliqueUrl);
+          } else {
+            urlDeferred.resolve(hutmap.STATIC_URL + 'hutmap/img/no-image-available.gif');
+          }
+        }
+        return urlDeferred.promise;
+      }
+
+      HutImg.getObliques = function(hut) {
+        var obliquesDeferred = $q.defer();
+        var obliques = [];
+        var obliqueUrl = getObliqueImgUrl(hut);
+        if (obliqueUrl) {
+            obliques.push({
+              url: obliqueUrl,
+              creditUrl: hut.photo_credit_url,
+              name: hut.photo_credit_name,
+            });
+        }
+        obliquesDeferred.resolve(obliques);
+        return obliquesDeferred.promise;
+      }
+
+      return HutImg;
+    }];
+
+    return HutImgProvider;
+  }]);
+
+})();
---
+++ src/hutmap/static/hutmap/js/hutmap/services/MaxZoom.js
@@ -0,0 +1,34 @@
+(function () {
+'use strict';
+
+  angular.module('hutmap.services').
+
+  // access the google.maps.MaxZoomService
+  provider('MaxZoom', [function() {
+    var MaxZoomProvider = {}
+    var defaultMaximumZoom = 16;
+
+    MaxZoomProvider.$get = ['$q', function($q) {
+      var MaxZoom = {};
+
+      var maxZoomService = new google.maps.MaxZoomService();
+
+      MaxZoom.getMaxZoom = function(latLng) {
+        var deferred = $q.defer();
+        maxZoomService.getMaxZoomAtLatLng(latLng, function(response) {
+          if (response.status != google.maps.MaxZoomStatus.OK) {
+              deferred.resolve(defaultMaximumZoom);
+          } else {
+              deferred.resolve(response.zoom);
+          }
+        });
+        return deferred.promise;
+      }
+
+      return MaxZoom;
+    }];
+
+    return MaxZoomProvider;
+  }]);
+
+})();
