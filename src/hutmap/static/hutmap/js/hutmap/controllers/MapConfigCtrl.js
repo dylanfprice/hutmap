4bd0a0c24a31c0dda96d4d861408d6b8f864f015
Dylan Price <the.dylan.price@gmail.com>
2014-06-24 00:53:40 -0700

    Don't simulate click on markers updated.

--- src/hutmap/static/hutmap/js/hutmap/controllers/MapConfigCtrl.js
+++ src/hutmap/static/hutmap/js/hutmap/controllers/MapConfigCtrl.js
@@ -27,7 +27,9 @@
     // return proper google.maps.MapOptions for the given hut
     $scope.getMarkerOptions = function(hut) {
       var opts = {};
-      if ($scope.h.filteredHuts && hut.id in $scope.h.filteredHutIds) {
+      if ($scope.h.selectedHut !== null && $scope.h.selectedHut.id === hut.id) {
+          return angular.extend(opts, markerOptions.huts, markerOptions.selected);
+      } if ($scope.h.filteredHuts && hut.id in $scope.h.filteredHutIds) {
         return angular.extend(opts, markerOptions.filteredHuts);
       } else {
         return angular.extend(opts, markerOptions.huts);
