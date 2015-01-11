5ed9e43ec451aca9bdca4391bd35934e5fe4aea3
Dylan Price <the.dylan.price@gmail.com>
2015-01-10 23:58:57 -0800

    Update command to take file argument

--- huts/management/commands/dumphutsjson.py
+++ huts/management/commands/dumphutsjson.py
@@ -1,9 +1,18 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 from huts.utils import export
 
+
 class Command(BaseCommand):
-    args = ''
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--file',
+            help='Write to file instead of stdout'
+        ),
+    )
     help = 'Dumps the huts, agencies, and regions in the json api format.'
 
     def handle(self, *args, **options):
-        print(export.db_as_json().encode('utf-8'))
+        out = options['file'] or self.stdout
+        out.write(export.db_as_json().encode('utf-8'))
