6fedddf54200d4fcd9a5fac4946311be0abb80f1
Dylan Price <the.dylan.price@gmail.com>
2015-05-15 16:35:25 -0700

    Use proper setting for static files in development

--- hutmap/urls.py
+++ hutmap/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.conf.urls.static import static
 from django.contrib.gis import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from huts.urls import hut_patterns, api_patterns
 
 admin.autodiscover()
@@ -19,5 +21,5 @@ urlpatterns += patterns('',
 )
 
 # serve static and media files during development
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
